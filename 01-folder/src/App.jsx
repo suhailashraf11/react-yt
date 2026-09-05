a1165394fed3ce244e8fa9b8023ec5d7c0716a24
Suhail A <suhailashraf282@gmail.com>
2026-09-06 00:43:50 +0530

    01 Folder Done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+  return ( 
+    <>
+    <div id="parent">
+      <h1 id="child1">Hello</h1>
+      <h1 id="child2">Hello2</h1>
+    </div>
+
+    <div id="chacha"></div>
+    </>
+  )
+}
+
+export default App
